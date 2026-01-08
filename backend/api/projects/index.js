c0cae92f407772c6f2788482dd6b3054a6f1e45b
MarsNunez <mars.nunez.arenas@gmail.com>
2026-01-08 12:16:33 -0500

    Adding changes

---
+++ backend/api/projects/index.js
@@ -0,0 +1,7 @@
+import app from "../../app.js";
+import { connectToDatabase } from "../../lib/db.js";
+
+export default async function handler(req, res) {
+  await connectToDatabase();
+  return app(req, res);
+}
